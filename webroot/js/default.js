7fe4cd6ccb56cc6ceae7fbc1200f033182e2b2b9
kyabetsuda <TsudaJun@tsudajun-no-MacBook-Air.local>
2019-02-11 13:31:25 +0900

    first commit

--- webroot/js/default.js
+++ webroot/js/default.js
@@ -206,8 +206,8 @@ function insertHtmlForArticleList(containerClassName, jsonData){
 function makeHtmlForArticleList(article){
   return '<div class="col-sm-4 mb-1 cardWrapper">'
     + '<div class="card mb-3" style="max-width: 25rem;">'
-    + '<a href="/articles/post?no=' + article.id + '">'
-    + '<img style="max-height:40vh; width: auto" class="card-img-top" src="/img/uploaded/' + article.thumbnail + '">'
+    + '<a style="" href="/articles/post?no=' + article.id + '">'
+    + '<img style="max-height: 50rem" class="card-img-top" src="/img/uploaded/' + article.thumbnail + '">'
     + '</a>'
     + '<div class="card-body text-center">'
     + '<span>' + article.title + '</span>'
