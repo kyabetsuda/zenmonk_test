f1b5e5a21bd4ad86548b9e7003bfc209bef9deb8
kyabetsuda <junn135246@icloud.com>
2019-07-07 16:01:34 +0900

    commit

--- webroot/js/default.js
+++ webroot/js/default.js
@@ -371,7 +371,8 @@ function sampleDate(date, format) {
 
 /* 最初の文を取得 */
 function getFirstSentenceFromStr(str){
-  return str.match(/^.*。/m);
+  // return str.match(/^.*。/m);
+  return str.substr(0, 30);
 }
 
 /********************************************************************************************
